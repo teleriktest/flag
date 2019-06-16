83b30c0724da7cc03137b1953064d3cb56c2f508
unknown <you@example.com>
2019-06-16 18:26:39 +0300

    ai

--- game.js
+++ game.js
@@ -123,16 +123,92 @@ class Player{
 
 class PlayerHuman extends Player{
     ai(){
-        if (isKeyPressed[87] && isKeyPressed[65]) this.moveTowards(this.x - 1, this.y-1); 
-        else if (isKeyPressed[87] && isKeyPressed[68]) this.moveTowards(this.x + 1, this.y-1); 
-        else if (isKeyPressed[83] && isKeyPressed[65]) this.moveTowards(this.x - 1, this.y+1); 
-        else if (isKeyPressed[83] && isKeyPressed[68]) this.moveTowards(this.x + 1, this.y+1); 
-        else if (isKeyPressed[65]) this.moveTowards(this.x - 1, this.y); 
-        else if (isKeyPressed[87]) this.moveTowards(this.x, this.y-1); 
-        else if (isKeyPressed[68]) this.moveTowards(this.x+1, this.y); 
+        if (isKeyPressed[87] && isKeyPressed[65]) this.moveTowards(this.x - 1, this.y-1);
+        else if (isKeyPressed[87] && isKeyPressed[68]) this.moveTowards(this.x + 1, this.y-1);
+        else if (isKeyPressed[83] && isKeyPressed[65]) this.moveTowards(this.x - 1, this.y+1);
+        else if (isKeyPressed[83] && isKeyPressed[68]) this.moveTowards(this.x + 1, this.y+1);
+        else if (isKeyPressed[65]) this.moveTowards(this.x - 1, this.y);
+        else if (isKeyPressed[87]) this.moveTowards(this.x, this.y-1);
+        else if (isKeyPressed[68]) this.moveTowards(this.x+1, this.y);
         else if (isKeyPressed[83]) this.moveTowards(this.x, this.y+1);
         if (isMousePressed) this.shoot(mouseX, mouseY);
     }
+}
+
+class PlayerDaniel extends Player{
+  constructor(team, ind) {
+    super(team, ind);
+    this.stage = 1
+  }
+  ai(){
+    if (this.team == 0) {
+      if (this.stage == 1) {
+        this.moveTowards(this.x - 1, this.y + 1);
+        if (this.x <= -110) this.stage = 2;
+      }
+      if (this.stage == 2) {
+        this.moveTowards(this.x, 720);
+        if (this.y >= 710) this.stage = 3;
+      }
+      if (this.stage == 3) {
+        this.moveTowards(710, this.y);
+        if (this.x >= 700) this.stage = 4;
+      }
+      if (this.stage == 4) {
+        this.moveTowards(this.x, 500);
+        if (this.flag == 1) this.stage = 5;
+      }
+      if (this.stage == 5) {
+        this.moveTowards(this.x, 720);
+        if (this.y >= 710) this.stage = 6;
+      }
+      if (this.stage == 6) {
+        this.moveTowards(-120, this.y);
+        if (this.x <= -110) this.stage = 7;
+      }
+      if (this.stage == 7) {
+        this.moveTowards(this.x, 100);
+        if (this.y <= 110) this.stage = 8;
+      }
+      if (this.stage == 8) {
+        this.moveTowards(100, this.y);
+        if (this.flag == 0) this.stage = 1;
+      }
+    } else {
+      if (this.stage == 1) {
+        this.moveTowards(this.x, 720);
+        if (this.y >= 710) this.stage = 2;
+      }
+      if (this.stage == 2) {
+        this.moveTowards(-120, this.y);
+        if (this.x <= -110) this.stage = 3;
+      }
+      if (this.stage == 3) {
+        this.moveTowards(this.x, 90);
+        if (this.y <= 100) this.stage = 4;
+      }
+      if (this.stage == 4) {
+        this.moveTowards(100, this.y);
+        if (this.flag == 1) this.stage = 5;
+      }
+      if (this.stage == 5) {
+        this.moveTowards(-120, this.y);
+        if (this.x <= -110) this.stage = 6;
+      }
+      if (this.stage == 6) {
+        this.moveTowards(this.x, 720);
+        if (this.y >= 710) this.stage = 7;
+      }
+      if (this.stage == 7) {
+        this.moveTowards(710, this.y);
+        if (this.x >= 700) this.stage = 8;
+      }
+      if (this.stage == 8) {
+        this.moveTowards(this.x, 500);
+        if (this.flag == 0) this.stage = 1;
+      }
+    }
+  }
 }
 
 class Flag{
@@ -213,7 +289,7 @@ class Bullet{
     }
 }
 
-var w = [], p = [new PlayerHuman(0, 0), new Player(1, 1)];
+var w = [], p = [new PlayerHuman(0, 0), new PlayerDaniel(1, 1)];
 var flag = [new Flag(0), new Flag(1)];
 var b = [];
 var score = [0, 0];
